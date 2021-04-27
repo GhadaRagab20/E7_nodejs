c468aa4dd4b64a9d80371cd174249fac31482d2c
GhadaRagab20 <62110252+GhadaRagab20@users.noreply.github.com>
2021-04-27 22:27:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post('/api/courses/create'   , (req , res) => {
     const joiCourses = Joi.object({
         name:Joi.string().min(5).required(),
         code:Joi.string().length(6).required(),
-        desc:Joi.string().min(0).max(200)
+        desc:Joi.string().allow(null,"").max(200)
     })
     const rst= joiCourses.validate(req.body);
      if (rst.error) return  res.status(400).send(error.details[0].message) ;
